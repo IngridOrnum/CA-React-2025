28a8eafeb92688c81c9f8a468b3e4e1374ee8681
Ingrid Ornum <ingrid.ornum@gmail.com>
2025-03-06 13:02:05 +0100

    Fix: fixed half star rating

--- src/components/Rating/StarRating.jsx
+++ src/components/Rating/StarRating.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import styles from './StarRating.module.css'
+import styles from './StarRating.module.css';
 
-export function StarRating({rating}) {
+export function StarRating({ rating }) {
     const fullStar = Math.floor(rating);
     const halfStar = (rating % 1) >= 0.5 ? 1 : 0;
     const emptyStar = 5 - fullStar - halfStar;
 
     return (
-        <div className={"StarRating"}>
+        <div>
             {Array(fullStar).fill().map((_, index) => (
-                <span key={index} className={`${styles.star}`}>★</span>
+                <span key={index} className={styles.star}>★</span>
             ))}
             {halfStar === 1 && (
-                <span key={"half-star"} className={`${styles.star} ${styles.halfStar}`}>★</span>
+                <span key="half-star" className={`${styles.star} ${styles.halfStar}`}>★</span>
             )}
             {Array(emptyStar).fill().map((_, index) => (
-                <span key={index + fullStar + halfStar} className={`${styles.star} ${styles.emptyStar}`}>★</span>
+                <span key={index} className={`${styles.star} ${styles.emptyStar}`}>★</span>
             ))}
         </div>
-    )
+    );
 }
