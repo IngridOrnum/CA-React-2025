335c4361bf0eb2cc8c71c43e8f00d6457e62dd10
Ingrid Ornum <ingrid.ornum@gmail.com>
2025-03-10 14:17:47 +0100

    Chore: finished footer styling

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,10 +1,24 @@
 import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 export function Footer() {
+    const [year, setYear] = useState(new Date().getFullYear());
+
+    useEffect(() => {
+        setYear(new Date().getFullYear());
+    }, []);
+
     return (
         <>
-            <footer className={"flex justify-center p-10 bg-gray-200"}>
-                <Link className={"font-bold text-lg"} to={"/"}>Lux Shop</Link>
+            <footer className={"flex flex-col items-center justify-center p-10 bg-[#55220020]"}>
+                <div className={"flex flex-col gap-10 items-center"}>
+                    <Link className={"font-logo text-2xl text-[#552200]"} to={"/"}>lux shop</Link>
+                    <div className={"flex flex-col gap-6 items-center font-title-bold font-light text-xl"}>
+                        <Link to={"/"}>Home</Link>
+                        <Link to={"/contact"}>Contact</Link>
+                    </div>
+                    <p className={"font-title-bold font-light"}>{`© lux shop ${year}. All Rights Reserved.`}</p>
+                </div>
             </footer>
         </>
     )
